0166acb08f565d9dfa380e4cde037c3dc46d3b40
Daniel González <dgonzalez@telesign.com>
2025-04-09 17:03:29 -0500

    EOA-933: add colors constants

---
+++ src/stories/foundation/constants/colors.ts
@@ -0,0 +1,93 @@
+export const PRIMARY_COLORS = [
+  'primary-base',
+  'primary-light',
+  'primary-lighter',
+  'primary-dark',
+  'primary-darker',
+];
+
+export const ACCENT_COLORS = [
+  'accent-base',
+  'accent-light',
+  'accent-lighter',
+  'accent-dark',
+  'accent-darker',
+];
+
+export const NEUTRAL = [
+  'neutral-25',
+  'neutral-50',
+  'neutral-100',
+  'neutral-200',
+  'neutral-300',
+  'neutral-400',
+  'neutral-500',
+  'neutral-600',
+  'neutral-700',
+  'neutral-800',
+  'neutral-900',
+];
+
+export const BASE = ['base-alt', 'base-light', 'base-lighter', 'base-dark', 'base-darker'];
+
+export const BLACK_WHITE = ['white', 'black'];
+
+export const SUCCESS = [
+  'success-base',
+  'success-light',
+  'success-lighter',
+  'success-dark',
+  'success-darker',
+];
+
+export const WARNING = [
+  'warning-base',
+  'warning-light',
+  'warning-lighter',
+  'warning-dark',
+  'warning-darker',
+];
+
+export const DANGER = [
+  'danger-base',
+  'danger-light',
+  'danger-lighter',
+  'danger-dark',
+  'danger-darker',
+];
+
+export const INFORMATION = [
+  'information-base',
+  'information-light',
+  'information-lighter',
+  'information-dark',
+  'information-darker',
+];
+
+export const FOCUS = ['focus'];
+
+export const UI_COLORS = [
+  'ui-default',
+  'ui-inverse',
+  'ui-primary',
+  'ui-accent',
+  'ui-success',
+  'ui-warning',
+  'ui-danger',
+  'ui-info',
+];
+
+export const TEXT_COLORS = [
+  'text-default',
+  'text-default-light',
+  'text-default-darker',
+  'text-inverse',
+  'text-primary',
+  'text-accent',
+  'text-success',
+  'text-warning',
+  'text-danger',
+  'text-information',
+];
+
+export const BACKGROUND_COLORS = ['bg-default', 'bg-inverse', 'bg-primary', 'bg-overlay'];
