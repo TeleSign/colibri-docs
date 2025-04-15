08564e9853750a1013b4688983243194d84b7323
Laura Felix <lfelix@telesign.com>
2025-04-15 10:31:25 -0400

    Revert "EOA-928: Updated the icon size in the badge component to be 12px as per Nicolas"
    This reverts commit 6780ed0c422856e45e7cf4f2229107c90aaa3538.

--- src/stories/components/ColBadge.stories.tsx
+++ src/stories/components/ColBadge.stories.tsx
@@ -88,7 +88,7 @@ type Story = ColibriStory<StoryArgs>;
 export const Default: Story = {
     render: args => html`
     <col-badge .variant=${args.variant} ?isDisabled=${args.isDisabled}> 
-        <col-icon slot="icon" name=${args.badgeIconName} size="12px"></col-icon>
+        <col-icon slot="icon" name=${args.badgeIconName} size="20px"></col-icon>
         ${args.badgeText}
     </col-badge>
     `,
@@ -111,7 +111,7 @@ export const SuccessVariantAndNoIcon: Story = {
 export const WarningVariantAndNoText: Story = {
     render: () => html`
     <col-badge variant="warning"> 
-        <col-icon slot="icon" name="home" size="12px"></col-icon>
+        <col-icon slot="icon" name="trash" size="20px"></col-icon>
     </col-badge>
     `,
 };
