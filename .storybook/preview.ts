6ef650538a708f94a78cb2d99ec6068c034bd23b
Daniel González <dgonzalez@telesign.com>
2025-04-25 11:02:33 -0500

    EOA-933: add courier font import

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -6,6 +6,7 @@ import type { Preview, WebComponentsRenderer } from '@storybook/web-components';
 import { ColBadge, registerColibriComponents } from '@tls-ds/colibri';
 import { ColIcon } from '@tls-ds/colibri-icons';
 import '@tls-ds/colibri/styles/fonts/segoe-ui.css';
+import '@tls-ds/colibri/styles/fonts/courier.css';
 import '@tls-ds/colibri/styles/global.css';
 import '@tls-ds/colibri/styles/theme-default.css';
 import '@tls-ds/colibri/styles/theme-massive.css';
