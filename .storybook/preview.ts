5a27725b0e3919eedd9af98f0f7faf6d7d9a53e8
Jhoy Berrocal <jberrocal@telesign.com>
2025-04-28 09:38:04 -0500

    Implement register all declared components

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -3,14 +3,15 @@ import { createElement } from 'react';
 import type { DecoratorFunction } from '@storybook/csf';
 import { DocsContainer } from '@storybook/blocks';
 import type { Preview, WebComponentsRenderer } from '@storybook/web-components';
-import { registerColibriComponents, ColBadge, ColDivider } from '@tls-ds/colibri';
+import { registerColibriComponents, registerAllComponents } from '@tls-ds/colibri';
 import { ColIcon } from '@tls-ds/colibri-icons';
 import '@tls-ds/colibri/styles/global.css';
 import '@tls-ds/colibri/styles/theme-default.css';
 import '@tls-ds/colibri/styles/theme-massive.css'
 import '@/styles/globals.css';
 
-registerColibriComponents([ColIcon, ColBadge, ColDivider]);
+registerAllComponents();
+registerColibriComponents([ColIcon]);
 
 /**
  * Type definitions for style properties.
