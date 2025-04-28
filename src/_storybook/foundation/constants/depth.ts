de7b8af7d0c577fc0d75f671c6645dcd6597a1ee
Daniel González <dgonzalez@telesign.com>
2025-04-28 16:20:44 -0500

    EOA-933: general refactoring

---
+++ src/_storybook/foundation/constants/depth.ts
@@ -0,0 +1,19 @@
+export const DEPTH = {
+  'box-shadow': {
+    xs: '0 1px 2px 0 rgba(var(--col-theme-black-rgb), .15)',
+    s: '0 2px 4px 0 rgba(var(--col-theme-black-rgb), .15)',
+    m: '0 4px 8px 0 rgba(var(--col-theme-black-rgb), .15)',
+    l: '0 8px 12px 0 rgba(var(--col-theme-black-rgb), .15)',
+    xl: '0 12px 16px 0 rgba(var(--col-theme-black-rgb), .15)',
+    'xs-inverse': '0 -1px 2px 0 rgba(var(--col-theme-black-rgb), .15)',
+    's-inverse': '0 -2px 4px 0 rgba(var(--col-theme-black-rgb), .15)',
+    'm-inverse': '0 -4px 8px 0 rgba(var(--col-theme-black-rgb), .15)',
+    'l-inverse': '0 -8px 12px 0 rgba(var(--col-theme-black-rgb), .15)',
+    'xl-large-inverse': '0 -12px 16px 0 rgba(var(--col-theme-black-rgb), .15)',
+    active:
+      '0 0 0 2px var(--col-theme-focus), inset 0 1px 2px 0 rgba(var(--col-theme-black-rgb), .15)',
+    focus: '0 0 0 2px var(--col-theme-focus)',
+    inset: 'inset 0 1px 2px 0 rgba(var(--col-theme-black-rgb), .15)',
+    plain: '0, 0, 0, 0 rgba(var(--col-theme-black-rgb), .15)',
+  },
+};
---
+++ src/_storybook/foundation/constants/radius.ts
@@ -0,0 +1,12 @@
+export const RADIUS = {
+  size: {
+    none: '0px',
+    xs2: '2px',
+    xs: '4px',
+    s: '8px',
+    m: '12px',
+    l: '16px',
+    xl: '24px',
+    full: '360px',
+  },
+};
