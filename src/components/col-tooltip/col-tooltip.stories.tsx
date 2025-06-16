682cdf7fc60e4a258be4dfe9ad8aa87d403381eb
Camilo Tatis <@telesign.com>
2025-06-16 15:40:50 -0500

    EOA 1011 MD Document and Default story

---
+++ src/components/col-tooltip/col-tooltip.stories.tsx
@@ -0,0 +1,77 @@
+import { html } from 'lit';
+import type { ColibriStoryMeta, ColibriStory } from '@/types/storybook';
+import { formatCodeString } from '@/utils/formatters';
+import { TOOLTIP_POSITIONS } from '@telesign/colibri';
+
+type StoryArgs = {
+  multiline: Boolean;
+  width: Number;
+  position: TOOLTIP_POSITIONS;
+};
+
+const meta = {
+  title: 'Atoms/Tooltip',
+  component: 'col-tooltip',
+  parameters: {
+    docs: {
+      source: {
+        excludeDecorators: true,
+        transform: formatCodeString,
+      },
+    },
+  },
+  argTypes: {
+    multiline: {
+      control: 'boolean',
+      description: 'Determines if the text of the tooltip will fit in one or more lines',
+      table: {
+        type: { summary: 'boolean' },
+        defaultValue: { summary: 'false' },
+        category: 'Core',
+      },
+    },
+    width: {
+      control: 'number',
+      description: 'Defines the container width and the multiline text behavior',
+      table: {
+        type: { summary: 'number' },
+        defaultValue: { summary: '200' },
+        category: 'Core',
+      },
+    },
+    position: {
+      control: 'select',
+      options: Object.keys(TOOLTIP_POSITIONS),
+      description: 'Determines the position that be displayed the tooltip',
+      table: {
+        type: { summary: 'string' },
+        defaultValue: { summary: 'top' },
+        category: 'Core',
+      },
+    },
+  },
+  args: {
+    multiline: true,
+    width: 300,
+    position: TOOLTIP_POSITIONS.Right,
+  },
+} satisfies ColibriStoryMeta<StoryArgs>;
+
+export default meta;
+
+type Story = ColibriStory<StoryArgs>;
+
+/**
+ * Default story showing a default tooltip. Use the controls panel to experiment with different values.
+ */
+export const Default: Story = {
+  render: ({ multiline, position, width }) => html`
+    <col-tooltip ?multiline=${multiline} width=${width} position=${position}>
+      <col-button variant="outlined">
+        <col-icon name="trash"></col-icon>
+        Delete Item
+      </col-button>
+      <div slot="tooltip-content">Tooltip message very very large in order to see multiline</div>
+    </col-tooltip>
+  `,
+};
