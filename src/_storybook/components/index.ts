779d0f378878af7d403b525b90a95ef0659c9ff9
Daniel González <dgonzalez@telesign.com>
2025-06-27 00:39:59 -0500

    PD-96662: add grid container utilities section

--- src/_storybook/components/index.ts
+++ src/_storybook/components/index.ts
@@ -2,5 +2,3 @@ export { Callout } from './Callout';
 export { Card } from './Card';
 export { Container } from './Container';
 export { Component } from './Component';
-export { GridCell } from './GridCell';
-export { GridExample } from './GridExample';
