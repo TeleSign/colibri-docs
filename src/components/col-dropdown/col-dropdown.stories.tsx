a303e3a70ff141795461bdd1f3126e0d33f2741f
Camilo Tatis <@telesign.com>
2025-07-10 12:36:44 -0500

    EOA 4033: Stories included in MD doc

--- src/components/col-dropdown/col-dropdown.stories.tsx
+++ src/components/col-dropdown/col-dropdown.stories.tsx
@@ -54,7 +54,7 @@ const meta = {
   args: {
     disabled: false,
     open: false,
-    align: 'end',
+    align: 'start',
   },
 } satisfies ColibriStoryMeta<StoryArgs>;
 
@@ -63,5 +63,83 @@ export default meta;
 type Story = ColibriStory<StoryArgs>;
 
 export const Default: Story = {
-  render: ({ disabled, open, align }) => html``,
+  render: ({ disabled, open, align }) => html`
+    <col-dropdown ?open=${open} ?disabled=${disabled} align=${align}>
+      <button slot="trigger">Toggle</button>
+      <div>Content</div>
+    </col-dropdown>
+  `,
+};
+
+export const DefaultWithComponents: Story = {
+  render: ({ disabled, open, align }) => html`
+    <div style="height: 150px;">
+      <col-dropdown ?open=${open} ?disabled=${disabled} align=${align}>
+        <col-button color="primary" slot="trigger">
+          Dropdown
+          <col-icon name="chevron-down"></col-icon>
+        </col-button>
+        <ul>
+          <p>Content 1</p>
+          <p>Content 2</p>
+          <p>Content 3</p>
+        </ul>
+      </col-dropdown>
+    </div>
+  `,
+};
+
+export const SmallListItems: Story = {
+  args: {
+    open: true,
+    disabled: false,
+    align: 'end',
+  },
+  render: args => html`
+    <div style="height: 100px; margin-left: 45%;">
+      <col-dropdown ?open=${args.open} ?disabled=${args.disabled} align=${args.align}>
+        <col-button color="primary" slot="trigger">
+          Click me
+          <col-icon name="chevron-down"></col-icon>
+        </col-button>
+        <p>Content 1</p>
+      </col-dropdown>
+    </div>
+  `,
+};
+
+export const MediumListItems: Story = {
+  args: {
+    open: true,
+    disabled: true,
+    align: 'start',
+  },
+  render: args => html`
+    <div style="height: 270px;">
+      <col-dropdown ?open=${args.open} ?disabled=${args.disabled} align=${args.align}>
+        <col-button color="default" slot="trigger"> Button with large text and no Icon </col-button>
+        <ul>
+          <p>Lorem ipsum dolor sit amet</p>
+          <p>Content 1</p>
+          <p>consectetur adipiscing elit</p>
+          <p>Content 2</p>
+          <p>sed do eiusmod tempor incididunt ut labore</p>
+          <p>Content 3</p>
+        </ul>
+      </col-dropdown>
+    </div>
+  `,
+};
+
+export const Disabled: Story = {
+  args: {
+    open: false,
+    disabled: true,
+    align: 'end',
+  },
+  render: args => html`
+    <col-dropdown ?open=${args.open} ?disabled=${args.disabled} align=${args.align}>
+      <col-button color="success" slot="trigger"> Toogle </col-button>
+    </col-dropdown>
+  `,
 };
