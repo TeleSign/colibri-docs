4076b5d61f2a4f5e990fc95533b0dcb833b0a2b7
Camilo Tatis <@telesign.com>
2025-08-04 15:45:14 -0500

    EOA 4101: Component preview stories

--- src/components/col-banner/col-banner.stories.tsx
+++ src/components/col-banner/col-banner.stories.tsx
@@ -83,3 +83,60 @@ export const Default: Story = {
       </col-group>
     </col-banner>`,
 };
+
+export const OnlyContent: Story = {
+  args: {
+    variant: 'success',
+  },
+  render: args =>
+    html`<col-banner
+      variant=${args.variant}
+      duration=${ifDefined(args.duration)}
+      @banner-closed=${args.onBannerClosed}
+    >
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+    </col-banner>`,
+};
+
+export const WithContentActions: Story = {
+  args: {
+    variant: 'warning',
+  },
+  render: args =>
+    html`<col-banner
+      variant=${args.variant}
+      duration=${ifDefined(args.duration)}
+      @banner-closed=${args.onBannerClosed}
+    >
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+      <div slot="actions">
+        <col-button variant="plain"> Accept </col-button>
+        <col-button color="danger"> Decline </col-button>
+      </div>
+    </col-banner>`,
+};
+
+export const WithTitleContentActions: Story = {
+  args: {
+    variant: 'danger',
+  },
+  parameters: {
+    __sb: {
+      width: '250px',
+    },
+  },
+  render: args =>
+    html`<col-banner
+      variant=${args.variant}
+      duration=${ifDefined(args.duration)}
+      @banner-closed=${args.onBannerClosed}
+    >
+      <span slot="title">Longer Title</span>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+      labore et dolore magna aliqua.
+      <col-group slot="actions">
+        <col-button color="danger"> Delete </col-button>
+        <col-button variant="plain"> Cancel </col-button>
+      </col-group>
+    </col-banner>`,
+};
