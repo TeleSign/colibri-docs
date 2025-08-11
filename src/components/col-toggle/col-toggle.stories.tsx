ea6dd18da301762fbd6235104f8ad622d202f2a0
Camilo Tatis <@telesign.com>
2025-08-11 08:56:52 -0500

    EOA 3954: Basic stories created

--- src/components/col-toggle/col-toggle.stories.tsx
+++ src/components/col-toggle/col-toggle.stories.tsx
@@ -93,3 +93,145 @@ export const Default: Story = {
     ></col-toggle>
   `,
 };
+
+export const WithLabel: Story = {
+  render: args => html`
+    <col-toggle
+      name=${args.name}
+      ?checked=${args.checked}
+      ?disabled=${args.disabled}
+      ?labelLeft=${args.labelLeft}
+    >
+      Enable Notifications
+    </col-toggle>
+  `,
+};
+
+export const WithLeftLabel: Story = {
+  args: {
+    labelLeft: true,
+  },
+  render: args => html`
+    <col-toggle
+      name=${args.name}
+      ?checked=${args.checked}
+      ?disabled=${args.disabled}
+      ?labelLeft=${args.labelLeft}
+    >
+      Receive Email Updates
+    </col-toggle>
+  `,
+};
+
+export const WithHelpText: Story = {
+  args: {
+    labelLeft: true,
+  },
+  render: args => html`
+    <col-toggle
+      name=${args.name}
+      ?checked=${args.checked}
+      ?disabled=${args.disabled}
+      ?labelLeft=${args.labelLeft}
+    >
+      Show Online Status
+      <span slot="help-text">This is a helper text</span>
+    </col-toggle>
+  `,
+};
+
+export const Checked: Story = {
+  args: {
+    checked: true,
+    labelLeft: true,
+  },
+  render: args => html`
+    <col-toggle
+      name=${args.name}
+      ?checked=${args.checked}
+      ?disabled=${args.disabled}
+      ?labelLeft=${args.labelLeft}
+    >
+      Enable Animations
+      <span slot="help-text">This is a helper text</span>
+    </col-toggle>
+  `,
+};
+
+export const Disabled: Story = {
+  args: {
+    checked: true,
+    disabled: true,
+  },
+  render: args => html`
+    <col-toggle
+      name=${args.name}
+      ?checked=${args.checked}
+      ?disabled=${args.disabled}
+      ?labelLeft=${args.labelLeft}
+    >
+      Dark Mode
+      <span slot="help-text">This is a helper text</span>
+    </col-toggle>
+  `,
+};
+
+export const DisabledChecked: Story = {
+  args: {
+    checked: true,
+    disabled: true,
+    labelLeft: true,
+  },
+  render: args => html`
+    <col-toggle
+      name=${args.name}
+      ?checked=${args.checked}
+      ?disabled=${args.disabled}
+      ?labelLeft=${args.labelLeft}
+    >
+      Two-Factor Authentication
+      <span slot="help-text">This is a helper text</span>
+    </col-toggle>
+  `,
+};
+
+export const InteractiveExample: Story = {
+  render: () => {
+    let checked = false;
+    let indeterminate = true;
+
+    const toggleState = () => {
+      if (indeterminate) {
+        indeterminate = false;
+        checked = true;
+      } else {
+        checked = !checked;
+      }
+      const checkbox = document.querySelector('#controlled-checkbox') as HTMLInputElement;
+      checkbox.checked = checked;
+      checkbox.indeterminate = indeterminate;
+      const stateDisplay = document.querySelector('#state-display') as HTMLInputElement;
+      stateDisplay.textContent = indeterminate
+        ? 'Indeterminate'
+        : checked
+          ? 'Checked'
+          : 'Unchecked';
+    };
+
+    return html`
+      <div class="checkbox-group">
+        <div class="checkbox-group-header">
+          <strong>Current state: </strong><span id="state-display">Indeterminate</span>
+        </div>
+        <div>
+          <col-checkbox id="controlled-checkbox" indeterminate @change=${toggleState}>
+            Click to toggle state
+          </col-checkbox>
+        </div>
+        <div>
+          <button class="toggle-button" @click=${toggleState}>Toggle state externally</button>
+        </div>
+      </div>
+    `;
+  },
+};
