5fe2ee355f0007430196ecf320175d9cd515ee66
Camilo Tatis <@telesign.com>
2025-09-01 15:06:31 -0500

    EOA 3926: Full integration story

--- src/components/col-dialog/col-dialog.stories.tsx
+++ src/components/col-dialog/col-dialog.stories.tsx
@@ -466,3 +466,132 @@ export const Multiple: Story = {
     `;
   },
 };
+
+export const FullIntegration: Story = {
+  render: args => {
+    const ID1 = `primary-full-dialog-${new Date().getTime()}`;
+    const ID2 = `secondary-full-dialog-${new Date().getTime()}`;
+
+    const handleButtonClick = () => {
+      const primaryDialog = document.getElementById(ID1);
+      if (primaryDialog) {
+        primaryDialog.setAttribute('active', 'true');
+      }
+    };
+
+    const handleSaveButtonClick = () => {
+      const form = document.getElementById('dialog-form') as HTMLFormElement;
+      const avatar = document.getElementById('user-avatar');
+      const formData = new FormData(form);
+      const data = Object.fromEntries(formData.entries());
+      avatar?.setAttribute('name', data.username);
+    };
+
+    const handleNotifications = () => {
+      const toggle = document.getElementById('enable-notifications-toggle');
+      if (toggle?.hasAttribute('checked')) {
+        const secondaryDialog = document.getElementById(ID2);
+        if (secondaryDialog) {
+          secondaryDialog.setAttribute('active', 'true');
+        }
+      }
+    };
+
+    const handleConfirmDialog = e => {
+      const action = e.detail.action;
+      const toggle = document.getElementById('enable-notifications-toggle');
+      if (action === 'footer-confirm') {
+        toggle?.removeAttribute('checked');
+
+        const secondaryDialog = document.getElementById(ID2);
+        if (secondaryDialog) {
+          secondaryDialog.removeAttribute('active');
+        }
+      }
+      if (action === 'footer-cancel' || e.type === 'dialog-close') {
+        toggle?.setAttribute('checked', 'true');
+      }
+    };
+
+    return html`
+      <div>
+        <h3>Account Settings</h3>
+        <div style="width: 400px;">
+          <col-typography variant="subheading">Personal Data</col-typography>
+          <div style="display: flex; justify-content: space-between;">
+            <col-avatar id="user-avatar" name="John Doe" variant="full"></col-avatar>
+            <col-button @click=${handleButtonClick} color="primary">Change Username</col-button>
+          </div>
+          <col-dialog
+            id=${ID1}
+            ?active=${args.active}
+            layout=${args.layout}
+            size=${args.size}
+            width=${args.width || null}
+            height=${args.width || null}
+            ?keepopen=${args.keepOpen}
+            @dialog-action=${args.onDialogAction}
+            @dialog-close=${args.onDialogClose}
+            @dialog-prevented=${args.onDialogPrevented}
+          >
+            <col-modal-header slot="header" title="Change Personal Data"></col-modal-header>
+
+            <p>
+              Please fill the text input in order to change your username. Then click the Save
+              button in order to continue.
+            </p>
+            <form id="dialog-form">
+              <col-text-field
+                id="username"
+                name="username"
+                label="Username"
+                required
+              ></col-text-field>
+            </form>
+
+            <col-modal-footer slot="footer">
+              <col-button slot="actions" data-action="cancel"> Cancel </col-button>
+              <col-button
+                slot="actions"
+                data-action="confirm"
+                color="primary"
+                @click=${handleSaveButtonClick}
+              >
+                Save
+              </col-button>
+            </col-modal-footer>
+          </col-dialog>
+        </div>
+        <div style="margin-top: 10px;">
+          <col-typography variant="subheading">Account Preferences</col-typography>
+          <col-group orientation="vertical">
+            <col-toggle
+              checked
+              name="enable-notifications-toggle"
+              id="enable-notifications-toggle"
+              @change=${handleNotifications}
+            >
+              Enable Notifications
+            </col-toggle>
+          </col-group>
+          <col-dialog
+            id=${ID2}
+            width="300"
+            height="180"
+            @dialog-close=${handleConfirmDialog}
+            @dialog-action=${handleConfirmDialog}
+          >
+            <col-modal-header slot="header" title="Dialog Title"></col-modal-header>
+            <p>Are you sure you want to disable notifications for your account?</p>
+            <col-modal-footer slot="footer">
+              <col-button slot="actions" data-action="cancel"> Cancel </col-button>
+              <col-button slot="actions" data-action="confirm" color="primary">
+                Confirm
+              </col-button>
+            </col-modal-footer>
+          </col-dialog>
+        </div>
+      </div>
+    `;
+  },
+};
