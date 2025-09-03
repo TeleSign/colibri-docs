00d78ac1f04d862b145e3b0e616d070c622e61d2
Daniel González <dgonzalez@telesign.com>
2025-09-03 09:20:49 -0500

    EOA-5325: fix component export

--- src/_storybook/components/index.ts
+++ src/_storybook/components/index.ts
@@ -1,5 +1,6 @@
 export { Callout } from './Callout';
 export { Card } from './Card';
 export { Container } from './Container';
+export { Component } from './Component';
 export { FormDemo } from './FormDemo';
 export { CodeBlock, highlightJsStyles } from './CodeBlock';
